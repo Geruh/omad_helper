efdd8c03d1d31e4e51072f9843a35a44af8147c6
MINORITY AFFAIRS AND DIVERSITY <omad@uw.edu>
2020-01-15 16:31:45 -0800

    saving... new pge

--- js/app.js
+++ js/app.js
@@ -2,9 +2,9 @@ $('.gallery-form').on('submit', function () {
 	var list = [];
 	var startValue = document.getElementById("start").value;
 	var endValue = document.getElementById("end").value;
-    for (var i = startValue; i <= endValue; i++) {
-    	list.push(i);
-    }
+	for (var i = startValue; i <= endValue; i++) {
+		list.push(i);
+	}
 	document.getElementById("result").value = "[gallery link='file' size='medium' ids= " + list.toString() + "]";
 	return false;
 });
@@ -14,9 +14,49 @@ $('.sidebar-form').on('submit', function () {
 	var articleTitle = document.getElementById("title").value;
 	var articleDate = document.getElementById("date").value;
 	var articleImg = document.getElementById("image").value;
-	document.getElementById("result").value = '<a href="' + articleLink + '"><img class="alignleft wp-image-25193 size-thumbnail" src="' + 
-												articleImg + '" alt="Link to article: ' + articleTitle + '" width="150" height="150" /></a><a class="widget-link" href="' + 
-												articleLink + '" target="_blank">' + articleTitle + '</a><small>' + articleDate + '</small>';
+	document.getElementById("result").value = '<a href="' + articleLink + '"><img class="alignleft wp-image-25193 size-thumbnail" src="' +
+		articleImg + '" alt="Link to article: ' + articleTitle + '" width="150" height="150" /></a><a class="widget-link" href="' +
+		articleLink + '" target="_blank">' + articleTitle + '</a><small>' + articleDate + '</small>';
+	return false;
+});
+
+$('.format-form').on('submit', function () {
+	let accord = `[accordion name='']\n[section title='More stories']\n`;
+	let text = document.getElementById("result").value;
+	text = text.split('[box]').join('');
+	text = text.split('[/box]').join('');
+	text = text.split(`[accordion name='']`).join('');
+	text = text.split(`[section title='More stories']`).join('');
+	text = text.trim() // do inside loop
+	tArray = text.split('[tile]');
+	tArray.shift();
+	res = `[box] \n`;
+	arch = "";
+	for (let i = 0; i < tArray.length; i++) {
+		if (i < 24) {
+			if (i % 3 == 0 && i != 0 && i != 6) {
+				res += `[/box] \n[box] \n\n`;
+			}
+			tArray[i] = tArray[i].trim();
+			res += `[tile] \n`;
+			res += tArray[i];
+			res += `\n\n`;
+			if (i == 5) {
+				res += '[/box]\n\n';
+				res += accord;
+				res += '\n[box]\n';
+			}
+			if (i == 23) {
+				res += '[/box]';
+			}
+		} else {
+			arch += '[tile]\n';
+			arch += tArray[i];
+			arch += '\n\n';
+		}
+	}
+	document.getElementById("result").value = res;
+	document.getElementById("extra").value = arch;
 	return false;
 });
 
@@ -35,4 +75,3 @@ $('.equity-form').on('submit', function () {
 	`[/tile]`;
 	return false;
 });
-;
